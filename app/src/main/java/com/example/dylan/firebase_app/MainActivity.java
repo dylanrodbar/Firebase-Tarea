4f5292d1503bf9351a1b6ec5caa6360664827521
dylanrodbar <dylanrodbar97@gmail.com>
2018-05-21 21:23:06 -0600

    pantalla de login y de registrarse

--- app/src/main/java/com/example/dylan/firebase_app/MainActivity.java
+++ app/src/main/java/com/example/dylan/firebase_app/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.dylan.firebase_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE = "com.example.dylan.firebase_app";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void openSignUpActivity(View view) {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, "");
+        startActivity(intent);
     }
 }
